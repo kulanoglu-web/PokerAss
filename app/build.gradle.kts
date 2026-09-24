d3a18504a8af8e0c06f80afad50bcdc62eec3ada
kulanoglu-web <kulanoglu@googlemail.com>
2026-09-24 14:54:14 +0200

    Fix JVM 17 build target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id(
 android {
  namespace="de.kulanoglu.pokerass"; compileSdk=35
  defaultConfig { applicationId="de.kulanoglu.pokerass"; minSdk=26; targetSdk=35; versionCode=1; versionName="0.1.0" }
+ compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
+ kotlinOptions { jvmTarget="17" }
  buildFeatures { compose=true }
 }
 dependencies {
